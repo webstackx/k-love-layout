a09c8a7751ff9296cf874e71bfa81d67eb3f200a
den-ganapathy <densk619@gmail.com>
2021-03-24 01:01:49 +0530

    den1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import { HomeWrapper } from '../styles/HomeStyle';
+import React, { Component } from "react";
+import { HomeWrapper } from "../styles/HomeStyle";
+import FeaturedBrand from "./../components/featuredBrand";
+import Product from "./../components/product";
 
 class Home extends Component {
-    render() {
-        return (
-            <HomeWrapper>
-                
-            </HomeWrapper>
-        )
-    }
+  render() {
+    return (
+      <HomeWrapper>
+        <FeaturedBrand />
+        <Product />
+      </HomeWrapper>
+    );
+  }
 }
 
 export default Home;
